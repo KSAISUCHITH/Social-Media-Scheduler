baaf5d194980678384001b11659b93ce4429fd5f
ksuchith131 <ksuchith131@gmail.com>
2026-07-26 18:47:00 +0530

    Created API for Social account creation

---
+++ server/config/Zernio.ts
@@ -0,0 +1,11 @@
+import {Zernio} from '@zernio/node';
+
+const zernio = new Zernio({
+
+    apiKey: process.env.ZERNIO_API_KEY || "",
+    baseURL: "https://zerni.com/api"
+}); // uses ZERNIO_API_KEY env var
+
+
+
+export default zernio;
